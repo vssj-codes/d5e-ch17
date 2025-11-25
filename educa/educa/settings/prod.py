f536568f3be67693366c448c63a35e11207f6c87
vssj-codes <vssjcodes@gmail.com>
2025-11-25 09:06:44 +0530

    ch17: using docker compose

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -1,3 +1,4 @@
+from decouple import config
 from .base import *
 
 DEBUG = False
@@ -7,7 +8,14 @@ ADMINS = [
 ALLOWED_HOSTS = ["*"]
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        NAME: BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
+REDIS_URL = "redis://cache:6379"
+CACHES["default"]["LOCATION"] = REDIS_URL
+CHANNEL_LAYERS["default"]["CONFIG"]["hosts"] = [REDIS_URL]
